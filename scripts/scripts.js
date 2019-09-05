e5512e7e3f2ac7a32d2035b1a863437d32525a72
Scott Metoyer <scott.metoyer@gmail.com>
2019-09-04 20:36:59 -0700

    Started interface selection

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+  // Add available MIDI interfaces to the select list
+  var select = document.getElementById('interfaceSelect');
+  // select.options[select.options.length] = new Option('Option 1', '1', false, false);
+});
